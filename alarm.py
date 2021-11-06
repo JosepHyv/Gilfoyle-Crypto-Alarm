4801ac8c3dd0e54378ee19a55f5ba64af473951c
JosepHyv <josephynimoto@gmail.com>
2021-11-06 02:26:58 -0600

    Finished

--- alarm.py
+++ alarm.py
@@ -11,30 +11,53 @@ import schedule
 import requests
 import playsound as player
 
-expectedValue = float(0)
+expectedValue = float(0) #only test
+urlApi = "https://api.coinbase.com/v2/exchange-rates?currency="
+crypto = "BTC" #default  gilfoyle is BTC
+coin = "USD" #default 
+times = 30 #time in seconds
 
 def sendMessage(coins, mess):
 	if sys.platform.lower() == "darwin":
 		os.system("""osascript -e 'display notification "{}" with title "{}"'
-                    """.format("Change In: " + str(coins), mess))
+                    """.format(str(coins) + " Change:", mess))
 	elif sys.platform.lower() == "linux":
-		os.system('notify-send "{}" "{}"'.format("Change in: " + str(coins), mess))
+		os.system('notify-send "{}" "{}"'.format(str(coins) + " Change:", mess))
 	else:
 		#procastination for windows :3 
 		pass
 
+def queryPrice():
+	"""
+		return value
+		float coin value
+	"""
+	qry = requests.get(str(urlApi + crypto))
+	value = qry.json()["data"]["rates"][coin]
+	return float(value)
 
 
-
-def alert(price):
-	sendMessage("BTC", "upper over $1200MXN")
-	player.playsound(os.path.join("media","./notification.mp3"))
+def plsound(status):
+	player.playsound(os.path.join("media", "./{}-notification.mp3".format(status)))
 	pass
 
-def main():
-	alert(0)
 
-	#algo 
+def launch(price):
+	"""
+		compare expectedValue  with price
+		return values 
+		-1 into the range
+		0 price down
+		1 price up
+	"""
+	if float(price) < float(expectedValue):			
+		sendMessage(crypto, """price dropped below {}{}""".format("{:.2f}".format(float(expectedValue - price)), coin))
+		return 0
+	elif float(price) > float(expectedValue):
+		sendMessage(crypto, """the price rose above {}{}""".format("{:.2f}".format(float(price - expectedValue)) , coin))
+		return 1
+	else:
+		return -1
 
-if __name__ == "__main__":
-	main()
+
+
